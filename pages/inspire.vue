9e43aa9aa132de4888cdc713bc797015d233cbd9
kimhaneui <haneui.kim@ybtour.com>
2022-01-11 19:24:33 +0900

    fetch

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,9 +7,21 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
+        <div>
+          <h2>카드 관리(Upload Image)</h2>
+          <p>{{image}}</p>
+        </div>
+        <div>
+          <h2>Specification</h2>
+          <p>{{prepare}}</p>
+        </div>
+        <div>
+          <h2>Sign In</h2>
+          <p>{{auth}}</p>
+        </div>
         <footer>
           <small>
-            <p>{{user}}</p>
+            <!-- <p>{{image}}</p> -->
           </small>
         </footer>
       </blockquote>
@@ -23,18 +35,38 @@ export default {
   name: 'InspirePage',
     data() {
     return {
-      user: '',
+      image: '',
+      prepare: '',
     }
   },
   methods: {
-    async fetchUser() {
-      const response = await axios.post('https://api.klipwallet.com/v2/wallet/image');
-      this.user = response.data;
-      console.log(this.user,'this.user')
-    }
+    fetchUser() {
+      const response = axios.post('https://api.klipwallet.com/v2/wallet/image');
+      this.image = response.data;
+    },
+    fetchPrepare() {
+      const response = axios.post('https://api.klipwallet.com/v2/a2a/prepare');
+      this.prepare = response.data;
+    },
+    fetchAuth() {
+      const response = axios.post('https://api.klipwallet.com/v2/partner/auth');
+      this.auth = response.data;
+      const url = 'https://api.klipwallet.com/v2/partner/auth';
+        const option = {
+            d: {"email":"ray.kim@groundx.xyz", "password":"C01069C9ABB6EA7DA49AE418A24BBEF3AD67170DDCD20AC7C76084A5A85E4057"},
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = fetch(url,option);
+        console.log(res,'res')
+    },
+
   },
   created() {
     this.fetchUser();
+    this.fetchPrepare();
+    this.fetchAuth();
   },
 }
 </script>
